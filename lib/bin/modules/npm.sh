97471208b72525b96fd81e1c95d369deb1e73056
kisstar <dwh.chn@foxmail.com>
2020-07-29 11:19:06 +0800

    fix($npm): 文档说明和实际安装操作不一致

--- lib/bin/modules/npm.sh
+++ lib/bin/modules/npm.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 npm_rc=~/.npmrc
-tip="Start generating configuration file"
 
 if is_conf_global
 then
-  npm_rc=~/.npmrc
-  tip="Generate configuration file to $HOME"
+  ensure_no_file $npm_rc
+  print_info "Link Npm related files to $HOME"
+  ln -s "$KS_CONFIG_ROOT/$KS_CONFIG_DIR_NAME/modules/npm/.npmrc" $npm_rc
 else
   npm_rc=$cwd/.npmrc
-  tip="Generate configuration file to $cwd"
+  ensure_no_file $npm_rc
+  print_info "Generate configuration file to $cwd"
+  cp "$KS_CONFIG_ROOT/$KS_CONFIG_DIR_NAME/modules/npm/.npmrc" $npm_rc
 fi
-
-# Check .npmrc
-ensure_no_file $npm_rc
-
-# Generate configuration file
-print_info $tip
-cp "$KS_CONFIG_ROOT/$KS_CONFIG_DIR_NAME/modules/npm/.npmrc" $npm_rc
--- modules/npm/README.md
+++ modules/npm/README.md
@@ -4,4 +4,4 @@ Npm 是 Node.js 默认的、用 JavaScript 编写的软件包管理系统。
 
 对 Npm 的配置支持添加全局或局部的 `.npmrc` 文件。
 
-全局安装将软连项目的 `modules/git/.gitmessage.txt` 文件到用户家目录，局部安装则是直接拷贝文件到执行命令的位置。
+全局安装将软连项目的 `modules/npm/.npmrc` 文件到用户家目录，局部安装则是直接拷贝文件到执行命令的位置。
